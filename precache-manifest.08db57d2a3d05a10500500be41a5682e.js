570d1795258219fc6b51b9e825ebd7257a547937
devresponses <ashok.adhikari18@gmail.com>
2018-11-04 09:54:33 -0500

    Updates

---
+++ precache-manifest.08db57d2a3d05a10500500be41a5682e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/website/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "1539ba91966c25af40b2",
+    "url": "/website/static/js/main.1539ba91.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/website/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "1539ba91966c25af40b2",
+    "url": "/website/static/css/main.b5eafafa.chunk.css"
+  },
+  {
+    "revision": "9bfe5d29ca62b03b46492a8b81d13002",
+    "url": "/website/index.html"
+  }
+];
